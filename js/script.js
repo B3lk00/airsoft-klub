250ec884cbc01da9d217d6c626d3e1b1cbc3fffa
B3lk00 <belmins1617@gmail.com>
2025-05-24 03:21:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-  const hero = document.querySelector(".hero");
-  hero.style.opacity = 0;
-  hero.style.transition = "opacity 1.5s ease-in-out";
-  setTimeout(() => {
-    hero.style.opacity = 1;
-  }, 200);
+  const el = document.getElementById("nekiElement");
+  if (el) {
+    el.style.color = "red";
+  }
 });
