74e5fe437a7cbde66e801bd47b1273ddb454accb
B3lk00 <belmins1617@gmail.com>
2025-05-25 02:51:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,4 +41,27 @@ function hideCategory() {
   document.querySelector('.gallery-folders').style.display = 'flex';
 }
 
+// LIGHTBOX
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll('.gallery-grid img');
+images.forEach(img => {
+  img.addEventListener('click', e => {
+    lightbox.classList.add('active');
+    const imgElement = document.createElement('img');
+    imgElement.src = img.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(imgElement);
+  });
+});
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
+
 
