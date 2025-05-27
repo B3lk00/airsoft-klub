a38dd5621899417c4847c2479e81dc194899e934
B3lk00 <belmins1617@gmail.com>
2025-05-27 23:10:52 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -146,14 +146,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-
-//hamburger
-function toggleMenu() {
-    const menu = document.querySelector('.mobile-menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-}
-
